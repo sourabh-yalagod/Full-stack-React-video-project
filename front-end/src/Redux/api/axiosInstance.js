d093ccdef1d810d3d16925171e8977f4e2833828
sourabh-yalagod <sourabhofficial99804@gmail.com>
2024-08-06 15:40:43 +0530

    cookies problem solved

--- front-end/src/Redux/api/axiosInstance.js
+++ front-end/src/Redux/api/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://videotube-auro.onrender.com", // your backend URL
   timeout: 10000, // optional: set a timeout for requests
+  withCredentials: true,
 });
 
 export default axiosInstance;
